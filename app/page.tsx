ec881a8abf81fcd97fbf80d1afddeb71d3dd9a0b
Muhammad Fakhar Khan <fakharkhan3312@gmail.com>
2025-10-07 00:39:19 +0500

    home page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import AddNewRecord from '@/components/AddNewRecord';
-import AIInsights from '@/components/AIInsights';
 import ExpenseStats from '@/components/ExpenseStats';
 import Guest from '@/components/Guest';
 import RecordChart from '@/components/RecordChart';
